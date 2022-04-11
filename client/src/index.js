7a75409a4f2e98d51d2544ccad19db6a60c46a47
Fareal Ahmed <farealahmed@gmail.com>
2022-04-12 02:44:06 +1000

    Add and showing list of data

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
@@ -7,28 +7,34 @@ import reportWebVitals from './reportWebVitals';
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
 import Invoice from "./routes/invoice";
+import TodoEntry from "./routes/todo-entry";
+import TodoList from "./routes/todo-list";
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />}>
-      <Route path=":invoiceId" element={<Invoice />} />
-    </Route>
-        <Route
-      path="*"
-      element={
-        <main style={{ padding: "1rem" }}>
-          <p>There's nothing here!</p>
-        </main>
-      }
-    />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route path="todo-entry" element={<TodoEntry />} />
+          <Route path="list-todo" element={<TodoList />} />
+
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There is nothing here</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 );
